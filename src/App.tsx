3cd8ce0c3265dc626e686df402c428193c341144
Kiran Namawar <kirannamawar@gmail.com>
2025-10-22 16:55:24 +0530

    feat: implement todo management functions (i.e, addToDo, updateToDo, deleteToDo) and define TODO type

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import ToDoList from "./components/ToDoList";
+import NewToDo from "./components/NewToDo";
+import type { TODO } from "./types/todo";
 
 function App() {
+    const [todos, setTodos] = useState<TODO[]>([]);
+
+    function addTodo(todo: TODO) {
+        setTodos([todo, ...todos]);
+    }
+
+    function updateToDo(todo: TODO) {
+        setTodos((prev) => prev.map((t) => (t.id === todo.id ? todo : t)));
+    }
+
+    function deleteToDo(id: string) {
+        setTodos((prev) => prev.filter((t) => t.id !== id));
+    }
+
     return (
-        <>
+        <div id="app">
             <Header />
             <main>
-                <ToDoList />
+                <NewToDo addTodo={addTodo} />
+                <ToDoList
+                    todos={todos}
+                    updateToDo={updateToDo}
+                    deleteToDo={deleteToDo}
+                />
             </main>
-        </>
+            <Footer />
+        </div>
     );
 }
 
