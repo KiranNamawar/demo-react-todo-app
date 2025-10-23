6e0c7af29b7d51356ce1ac8449159257c58c6f07
Kiran Namawar <kirannamawar@gmail.com>
2025-10-23 13:11:25 +0530

    Add link to source code on github in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,13 @@
 import { Github } from "lucide-react";
 
 function Footer() {
-    return (
-        <footer>
-            <a href="">
-                <Github /> <p>Source Code</p>
-            </a>
-        </footer>
-    );
+  return (
+    <footer>
+      <a href="https://github.com/KiranNamawar/demo-react-todo-app">
+        <Github /> <p>Source Code</p>
+      </a>
+    </footer>
+  );
 }
 
 export default Footer;
